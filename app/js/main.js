f241bcfce62891bd0c1b9857256de8594ff1d460
Roman <webroni2509@gmail.com>
2020-09-14 23:02:48 +0300

    surfhouse backup

---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+
+const clearBtn = document.querySelector('.clear-input');
+const searchInput = document.querySelector('.search-input');
+clearBtn.addEventListener('click', () => searchInput.value = '')
